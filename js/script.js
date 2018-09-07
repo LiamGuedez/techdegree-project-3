52d3bb643dc4b392d766e9251d8b96693a66b5f8
LiamGuedez <liamhayes9393@gmail.com>
2018-09-07 11:34:44 -0400

    Added new files
    -Added normalize.css
    -Added script.js
    -Set focus on the first text field
    -Created "Job Role" text field, inside the last option of the first drop-down menu

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//adds text field for last option inside the first drop-down menu
+const addJobTextField = () =>
+{
+  const basicFieldSet = document.querySelector('.basic');
+  const otherTextField = document.createElement('input');
+  otherTextField.type = 'text';
+  otherTextField.id = 'other-title';
+  otherTextField.placeholder = "Your Job Role";
+  otherTextField.style.display = 'none';
+
+  basicFieldSet.appendChild(otherTextField);
+
+  basicFieldSet.addEventListener('change', (event) => {
+    if(event.target.tagName === 'SELECT')
+    {
+      otherTextField.style.display = 'none';
+      if (event.target.value === 'other')
+      {
+          otherTextField.style.display = 'block';
+      }
+    }
+
+  });
+}
+
+//start with the first field selected on page load
+window.onload = function (){
+  const nameField = document.getElementById('name');
+  nameField.focus();
+}
+
+addJobTextField();
