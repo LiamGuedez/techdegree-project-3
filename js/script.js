6bb95887b4338a0888e9cc7173691dd0c15ce864
LiamGuedez <liamhayes9393@gmail.com>
2018-09-08 12:54:30 -0400

    Added functionality to the "T-Shirt" fieldset
    The t-shirt "color" menu
    - Is hidden on page load
    - Only displays the color options that match the design selected in the "design" menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//adds text field for last option inside the first drop-down menu
+//adds text field for last option inside the first dropdown menu
 const addJobTextField = () =>
 {
   const basicFieldSet = document.querySelector('.basic');
@@ -19,14 +19,66 @@ const addJobTextField = () =>
           otherTextField.style.display = 'block';
       }
     }
-
   });
 }
 
-//start with the first field selected on page load
-window.onload = function (){
-  const nameField = document.getElementById('name');
+////for the t-shirt "color" menu, only displays the color options
+// that match the design selected in the "design" menu
+const addTshirtFunctionality = () =>
+{
+  const shirtFieldSet = document.querySelector('.shirt');
+  const colorDropdown = document.querySelector('#color');
+
+  //helper function
+  displayIndeces = (array, start, end, value) =>
+  {
+    for (let x = start ; x <= end ; x++ )
+    {
+      array[x].style.display = value;
+    }
+  }
+
+  //helper function
+  const adjustSelectMenu = (start, end) =>
+  {
+    colorDropdown.style.display = 'block';
+    displayIndeces(colorDropdown, 0, 6, 'none');
+    displayIndeces(colorDropdown, start, end, 'block');
+    colorDropdown.selectedIndex = start;
+  }
+
+  //on page load, hide the color dropdown
+  window.addEventListener('load', () =>
+  {
+    colorDropdown.style.display = 'none';
+  });
+
+  shirtFieldSet.addEventListener('change', (event) =>
+  {
+    if(event.target.tagName === 'SELECT')
+    {
+      if (event.target.value === 'js puns')
+      {
+        adjustSelectMenu(1,3);
+      }
+      else if (event.target.value === 'heart js')
+      {
+        adjustSelectMenu(4,6);
+      }
+      else
+      {
+        colorDropdown.style.display = 'none';
+      }
+    }
+  });
+}
+
+//on page load, start with the first field selected
+window.onload = () =>
+{
+  const nameField = document.querySelector('#name');
   nameField.focus();
 }
 
 addJobTextField();
+addTshirtFunctionality();
