93eb6848ad720079747aeb151bede2de84f61e1e
LiamGuedez <liamhayes9393@gmail.com>
2018-09-17 19:22:18 -0400

    Checks if numbers were entered
    Added code to check if the user entered numbers for the credit card, zip code, and cvv fields.
    
    If the user does not enter a number in these fields, the form won't submit.

--- js/formValidation.js
+++ js/formValidation.js
@@ -34,16 +34,19 @@ const formValidation = () =>
       const error = [
         (document.querySelector('#name').value === ''),                                           //  error 0: empty name field
         (document.querySelector('#mail').value === ''),                                           //  error 1: empty email field
-        !validateEmail(document.querySelector('#mail').value),                                     //  error 2: invalid email format
+        !validateEmail(document.querySelector('#mail').value),                                    //  error 2: invalid email format
         !anyTrueValue(document.querySelectorAll('#activities label [type=checkbox]'), 'checked'), //  error 3: no checked checkboxes
         (document.querySelector('#payment').selectedIndex === 0),                                 //  error 4: no selection in payment dropdown
-        creditCard() && (document.querySelector('#cc-num').value === ''),                        //  error 5: empty credit card field
-        creditCard() && ( (document.querySelector('#cc-num').value.length < 13) ||                //  error 6: incorrect credit card value
+        creditCard() && (document.querySelector('#cc-num').value === ''),                         //  error 5: empty credit card field
+        creditCard() && ( (document.querySelector('#cc-num').value.length < 13) ||                //  error 6: too many/too few cc digits
                           (document.querySelector('#cc-num').value.length > 16)  ),
-        creditCard() && (document.querySelector('#zip').value === ''),                           //  error 7: empty zip code field
-        creditCard() && (document.querySelector('#zip').value.length !== 5),                      //  error 8: incorrect zip code
-        creditCard() && (document.querySelector('#cvv').value === ''),                           //  error 9: empty vin number field
-        creditCard() && (document.querySelector('#cvv').value.length !== 3),                      // error 10: incorrect vin value
+        creditCard() && !(Number.isInteger(Number(document.querySelector('#cc-num').value))),     //  error 7: cc entry is not a number
+        creditCard() && (document.querySelector('#zip').value === ''),                            //  error 8: empty zip code field
+        creditCard() && (document.querySelector('#zip').value.length !== 5),                      //  error 9: too many/too few zip digits
+        creditCard() && !(Number.isInteger(Number(document.querySelector('#zip').value))),        // error 10: zip entry is not a number
+        creditCard() && (document.querySelector('#cvv').value === ''),                            // error 11: empty cvv number field
+        creditCard() && (document.querySelector('#cvv').value.length !== 3),                      // error 12: too many/too few cvv digits
+        creditCard() && !(Number.isInteger(Number(document.querySelector('#cvv').value))),        // error 13: cvv entry is not a number
         ];
 
       anyTrueValue(error) === true ? event.preventDefault() : document.querySelector('form').submit();
